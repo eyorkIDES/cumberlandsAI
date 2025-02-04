6b9c69fa407ba75337d5c60462574579f024e7a2
eyorkIDES <130777181+eyorkIDES@users.noreply.github.com>
2025-02-04 09:45:19 -0500

    Add files via upload

--- website.py
+++ website.py
@@ -6,9 +6,21 @@ import time
 assistant_id = "asst_YkgNKU6zP0LuzqwI9cAlP05t"
 openai_api_key = st.secrets["DB_API_KEY"]
 
-# Show title and description.
-st.title("💬 UC AI Assistant")
-st.write("Ask anything to test the assistant's capabilities")
+# Set tab name (title) and favicon
+st.set_page_config(
+    page_title="UC AI Assistant",  # This sets the tab name
+    page_icon="favicon.png",  # This sets the favicon (must be a local file or URL)
+)
+
+# Create banner layout
+col1, col2 = st.columns([1, 2])  # Adjust ratio for image and text
+
+with col1:
+    st.image("logo.webp", width=1500)  # Display image
+
+st.title("AI Assistant")  # Assistant title
+
+st.write("Ask anything to test the assistant's capabilities")  # Description text
 
 # Password Authentication
 placeholder = st.empty()  # Create a placeholder
